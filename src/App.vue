399a20f52d549cd3497bffbf52e354afe5971735
Zer02 <abasdeo29@gmail.com>
2024-12-06 22:11:00 -0500

    updated files and reset firebase setup

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/Home.vue'
 </script>
 
 <style scoped>
